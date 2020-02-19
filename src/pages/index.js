d9e519b309404b8480c28103b21f4a20fc293d28
Noam Sauer-Utley <noamsauerutley@gmail.com>
2020-02-19 15:16:57 -0500

    Update links

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ import Github from '../components/github'
 import LinkedIn from '../components/linkedin'
 import Medium from '../components/medium'
 import Email from '../components/email'
-import Twitter from '../components/twitter'
+import Dev from '../components/dev'
 import SEO from "../components/seo"
 
 const IndexPage = () => (
@@ -14,8 +14,8 @@ const IndexPage = () => (
     <div style={{ margin: `0 auto`, marginTop: "8vh", maxHeight: `60vh`, textAlign: "center", display: "flex", flexDirection: "row", justifyContent: "space-around"}}>
       <a className="with_effect" style={{marginTop: "37vh"}} href="https://github.com/noamsauerutley"><Github /></a> 
       <a className="with_effect" href="https://www.linkedin.com/in/noamsauerutley/"><LinkedIn /></a>
-      <a className="with_effect" style={{marginTop: "25vh"}} href="https://twitter.com/thevoidbutcozy"><Twitter /></a> 
-      <a className="with_effect" style={{marginTop: "48vh"}} href="https://medium.com/@noamsauerutley"><Medium /></a> 
+      <a className="with_effect" style={{marginTop: "25vh"}} href="https://medium.com/@noamsauerutley"><Medium /></a> 
+      <a className="with_effect" style={{marginTop: "45vh"}} href="https://dev.to/noamsauerutley"><Dev /></a> 
       <Link className="with_effect" style={{marginTop: "10vh"}} to="/contact"><Email /></Link> 
 
     </div>
